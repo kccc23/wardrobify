a1105dde93c461d5e5a52426bdcbe7619ff6a110
Kaining <kainingchen2013@gmail.com>
2023-04-19 16:46:13 -0400

    Add get, post, delete

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -4,13 +4,20 @@ from common.json import ModelEncoder
 from .models import LocationVO, Hat
 from django.views.decorators.http import require_http_methods
 
-class HatListEncoder(ModelEncoder):
+class LocationVODetailEncoder(ModelEncoder):
+    model = LocationVO
+    properties = ["import_href"]
+
+class HatEncoder(ModelEncoder):
     model = Hat
     properties = [
         "fabric",
         "style_name",
         "color",
+        "picture",
+        "location",
     ]
+    encoders = { "location": LocationVODetailEncoder() }
 
 @require_http_methods(["GET", "POST"])
 def api_list_hats(request, location_vo_id=None):
@@ -21,7 +28,34 @@ def api_list_hats(request, location_vo_id=None):
             hats = Hat.objects.filter(location=location_vo_id)
         return JsonResponse(
             {"hats": hats},
-            encoder=HatListEncoder,
+            encoder=HatEncoder,
         )
     else: # "POST"
-        pass
+        content = json.loads(request.body)
+        try:
+            location = LocationVO.objects.get(import_href=content["location"])
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location"},
+                status=400,
+            )
+        hat = Hat.objects.create(**content)
+        return JsonResponse(
+            hat,
+            encoder=HatEncoder,
+            safe=False,
+        )
+
+@require_http_methods(["DELETE"])
+def api_hat(request, pk):
+    try:
+        hat = Hat.objects.get(id=pk)
+        hat.delete()
+        return JsonResponse(
+            hat,
+            encoder=HatEncoder,
+            safe=False,
+        )
+    except Hat.DoesNotExist:
+        return JsonResponse({"message": "Does not exist"})
