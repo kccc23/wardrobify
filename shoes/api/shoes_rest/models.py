3acb38531acf272efb51a1b0e339c9a6f1259a41
Jason <chiyoung.jang@mail.mcgill.ca>
2023-04-19 12:25:55 -0400

    Created Shoe Model

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ShoeVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    model_name = models.CharField(max_length=200)
+
+class Shoe(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture = models.URLField()
+    bin = models.ForeignKey(
+        ShoeVO,
+        related_name="shoe",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
