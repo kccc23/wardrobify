d1a3b5beafc6a0ab6c8f7bfc1a6a1ca824e0bb0c
Jason <chiyoung.jang@mail.mcgill.ca>
2023-04-19 15:19:23 -0400

    implemented GET and POST for shoes

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
-class ShoeVO(models.Model):
+#might need to add a few more properties to the VO
+class BinVO(models.Model):
     import_href = models.CharField(max_length=200, unique=True)
-    model_name = models.CharField(max_length=200)
 
 class Shoe(models.Model):
     manufacturer = models.CharField(max_length=200)
@@ -10,10 +11,13 @@ class Shoe(models.Model):
     color = models.CharField(max_length=200)
     picture = models.URLField()
     bin = models.ForeignKey(
-        ShoeVO,
+        BinVO,
         related_name="shoe",
         on_delete=models.CASCADE,
     )
 
     def __str__(self):
-        return self.name
+        return self.model_name
+
+    def get_api_url(self):
+        return reverse("api_show_show", kwargs={"id": self.id})
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+
+from common.json import ModelEncoder
+from .models import Shoe, BinVO
+
+
+class ShoeEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "manufacturer",
+        "model_name",
+        "color",
+    ]
+
+@require_http_methods(["GET", "POST"])
+def api_shoes(request, bin_id=None):
+    """
+    Collection RESTful API handler for list of shoe objects
+
+    GET:
+    Returns a list of shoes
+
+    POST:
+    Creates a new shoe
+    """
+    if request.method == "GET":
+        if bin_id==None:
+            shoes = Shoe.objects.all()
+        else:
+            shoes = Shoe.objects.filter(bin=bin_id)
+        return JsonResponse(
+            {"shoes": shoes},
+            encoder=ShoeEncoder,
+            safe=False,
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            import_href = f"api/bins/{bin_id}/"
+            binVO = BinVO.objects.get(import_href=import_href)
+
+        except BinVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid bin id"},
+                status=400,
+            )
+        shoe = Shoe.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeEncoder,
+            safe=False,
+        )
+
+# @require_http_methods(["DELETE", "GET", "PUT"])
+# def api_shoe(request, id):
+#     if request.method == "DELETE":
+#         try:
+#             shoe = Shoe.objects.get(id=id)
+#             shoe.delete()
+#             return JsonResponse(
+#                 shoe,
+#                 encoder=ShoeEncoder,
+#                 safe=False,
+#             )
+#         except Shoe.DoesNotExist:
+#             return JsonResponse({"message": "Does not exist"})
